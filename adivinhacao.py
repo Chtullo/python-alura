8fbab4e6c38424f5b7010b0b1d30cb285de704ea
Nicholas Castro <contabrpt2@hotmail.com>
2023-09-10 20:56:19 -0300

    Add files via upload
    corrigido redundância de parenteses

--- adivinhacao.py
+++ adivinhacao.py
@@ -10,7 +10,7 @@ print("você digitou ", chute_str)
 
 chute = int(chute_str)
 
-if(numero_secreto == chute):
+if numero_secreto == chute:
     print("você acertou!")
 else:
     print("você errou!")
