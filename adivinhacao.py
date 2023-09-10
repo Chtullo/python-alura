d9e4757c2ec1568e242b3217e9c7785c411103fb
Nicholas Castro <contabrpt2@hotmail.com>
2023-09-10 20:47:35 -0300

    Add files via upload

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,16 @@
+print("*********************************")
 print("Bem vindo no jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu numero: ")
+
+print("você digitou ", chute_str)
+
+chute = int(chute_str)
+
+if(numero_secreto == chute):
+    print("você acertou!")
+else:
+    print("você errou!")
