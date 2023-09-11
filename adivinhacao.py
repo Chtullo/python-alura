60b38ecbaf43f75da25dec90f223c1056afc9221
Nicholas Castro <contabrpt2@hotmail.com>
2023-09-10 21:05:55 -0300

    Add files via upload

--- adivinhacao.py
+++ adivinhacao.py
@@ -10,9 +10,18 @@ print("você digitou ", chute_str)
 
 chute = int(chute_str)
 
-if numero_secreto == chute:
+acertou     = numero_secreto == chute
+
+chute_maior = chute > numero_secreto
+
+chute_menor = chute < numero_secreto
+
+if acertou:
     print("você acertou!")
 else:
-    print("você errou!")
+    if chute_maior:
+        print("Você errou! O seu chute foi maior do que o numero secreto.")
+    elif chute_menor:
+        print("Você errou! O seu chute foi menor do que o numero secreto.")
 
 print("Fim do jogo.")
