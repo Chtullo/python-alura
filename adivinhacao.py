bcd478f76f586268324805c3e99ceecb4ace9ea0
Nicholas Castro <contabrpt2@hotmail.com>
2023-09-11 11:02:43 -0300

    Add files via upload
    ainda com um erro, mas gostaria de salvar este stage

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,25 +3,30 @@ print("Bem vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
+while rodada <= total_de_tentativas:
+
+    print("Tentativa: ", rodada, "de", total_de_tentativas)
 chute_str = input("Digite o seu numero: ")
-
 print("você digitou ", chute_str)
-
 chute = int(chute_str)
 
-acertou     = numero_secreto == chute
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
 
-chute_maior = chute > numero_secreto
-
-chute_menor = chute < numero_secreto
 
 if acertou:
     print("você acertou!")
 else:
-    if chute_maior:
+    if maior:
         print("Você errou! O seu chute foi maior do que o numero secreto.")
-    elif chute_menor:
+    elif menor:
         print("Você errou! O seu chute foi menor do que o numero secreto.")
+
+    rodada = rodada + 1
+
 
 print("Fim do jogo.")
