cf1da71475f7112dc199a0d98889907da1eb8718
Nicholas Castro <contabrpt2@hotmail.com>
2023-09-12 21:58:40 -0300

    corrigido erro do loop infinito

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,5 +1,5 @@
 print("*********************************")
-print("Bem vindo no jogo de Adivinhação!")
+print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
@@ -7,26 +7,24 @@ total_de_tentativas = 3
 rodada = 1
 
 while rodada <= total_de_tentativas:
+    print("Tentativa: {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-    print("Tentativa: ", rodada, "de", total_de_tentativas)
-chute_str = input("Digite o seu numero: ")
-print("você digitou ", chute_str)
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
-
-
-if acertou:
-    print("você acertou!")
-else:
-    if maior:
-        print("Você errou! O seu chute foi maior do que o numero secreto.")
-    elif menor:
-        print("Você errou! O seu chute foi menor do que o numero secreto.")
+    if acertou:
+        print("Você acertou!")
+        break  # Sai do loop quando o jogador acerta
+    else:
+        if maior:
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif menor:
+            print("Você errou! O seu chute foi menor do que o número secreto.")
 
     rodada = rodada + 1
-
 
 print("Fim do jogo.")
