a395b8244735c1a0338d891d602e949f8127d02f
Nicholas Castro <contabrpt2@hotmail.com>
2023-09-19 00:03:44 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -11,9 +11,10 @@ def jogar():
     while not enforcou and not acertou:
 
         chute = input("Qual letra? ")
+        chute = chute.strip()
         index = 0
         for letra in palavra_secreta:
-            if chute == letra:
+            if chute.upper == letra.upper():
                 print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
 
