eec2633668d832bf43f3816e26bfea5256ee30c9
Nicholas Castro <contabrpt2@hotmail.com>
2023-09-21 12:28:03 -0300

    Add files via upload

--- jogos/forca.py
+++ jogos/forca.py
@@ -6,10 +6,19 @@ def jogar():
     palavra_secreta = "banana"
 
     enforcou = False
-    acertou =False
+    acertou = False
 
-    while(not enforcou and not acertou):
-        print("Joogando ...")
+    while not enforcou and not acertou:
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper == letra.upper():
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando ...")
 
     print("Fim do jogo.")
 
